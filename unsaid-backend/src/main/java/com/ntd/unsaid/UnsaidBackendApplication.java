e02c177a59f0c818c843e0ec1337d45647f5b8eb
Nguyen Tien Dat <tie.dat2004@gmail.com>
2026-01-14 08:56:30 +0700

    init

---
+++ unsaid-backend/src/main/java/com/ntd/unsaid/UnsaidBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ntd.unsaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsaidBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsaidBackendApplication.class, args);
+	}
+
+}
---
+++ unsaid-backend/src/test/java/com/ntd/unsaid/UnsaidBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntd.unsaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsaidBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
