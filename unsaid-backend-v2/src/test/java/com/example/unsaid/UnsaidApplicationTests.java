cd6cd273a1ca2673c82ae0d515529a68bd6405d8
Nguyen Tien Dat <tie.dat2004@gmail.com>
2026-01-20 09:25:55 +0700

    migrate: monolith to modulith

---
+++ unsaid-backend-v2/src/test/java/com/example/unsaid/UnsaidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unsaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
